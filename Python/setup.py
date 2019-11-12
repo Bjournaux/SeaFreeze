58e5d4126d8327e618cd0ae851402c51a503e87f
pennythewho <pennespinoza+git@gmail.com>
2019-11-11 18:54:19 -0800

    update readme and bump post version

--- Python/setup.py
+++ Python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='SeaFreeze',
-    version='0.9.2.post1',
+    version='0.9.2.post2',
     author='pennythewho',
     author_email='who@pennythewho.com',
     description='thermodynamic properties of the phases of H₂O',
